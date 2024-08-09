6593d5213236af0475fde786d7c60d76fa2f7a12
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-08-09 13:20:42 +0530

    Update config.sh

--- arch/config.sh
+++ arch/config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Git Configuration
-export GIT_USERNAME="Sushrut1101"
-export GIT_EMAIL="guptasushrut@gmail.com"
+export GIT_USERNAME="sksayz5"
+export GIT_EMAIL="sksayz5@gmail.com"
 
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
